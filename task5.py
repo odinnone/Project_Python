5f65534c1c624c664f7cfc061b6bd32ef8e50681
odinnone <vladborovkov01@mail.ru>
2022-12-07 17:03:52 +0300

    add task5

--- task5.py
+++ task5.py
@@ -1,2 +1,6 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат. ¬ - Отрицание ⋁ - логическое "Или" ⋀ - логическое "И"
 
+for x in [True,False]:
+    for y in [True,False]:
+        for z in [True,False]:
+            print('NOT(',x,'AND',y,'AND',z,')=',not x or not y or not z)
