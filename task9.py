cb926fabd9d2bd111f8c62e7306c089c1e3c95e8
odinnone <vladborovkov01@mail.ru>
2022-12-12 18:26:51 +0300

    add task 9

--- task9.py
+++ task9.py
@@ -1,2 +1,13 @@
 # Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях.
 
+from random import randint
+numbers = []
+n = int(input('Введите кол-во элементов: '))
+for i in range(n):
+    numbers.append(randint (-n,n))
+print(numbers)
+x = int(input('Позиция первого элемента: '))
+y = int(input('Позиция второго элемента: '))
+for i in range(len(numbers)):
+    p = numbers[x -1]*numbers[y - 1]
+print(p)
